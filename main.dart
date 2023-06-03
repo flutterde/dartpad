7f46ee8c758dca735c192ea9d57bb485bbba6204
flutterde <71704747+flutterde@users.noreply.github.com>
2023-06-03 10:11:49 +0000

    -

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main() {
+  DateTime startDaate = DateTime.now();
+  print(startDaate);
+  DateTime? endTime;
+  for (int i = 0; i < 10000000000; i++) {
+    
+  }
+  endTime = DateTime.now();
+  print(endTime);
+}
